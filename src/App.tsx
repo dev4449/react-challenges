4aa0c66f661bcbe241ef2a1e9552de00a6c75f62
Mohsen Salahshour Majd <mohsen.salahshoor130@gmail.com>
2024-07-13 01:58:31 +0330

    Incomplete code

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import "@fontsource/ubuntu/300.css";
 import "@fontsource/ubuntu/400.css";
 import "@fontsource/ubuntu/500.css";
 import "@fontsource/ubuntu/700.css";
+import MobileNavbar from "./components/mobile-navbar/MobileNavbar";
 function App() {
   return (
     <>
-      <h1>Sphinx of black quartz, judge my vow.</h1>
+      <MobileNavbar />
     </>
   );
 }
---
+++ src/components/mobile-navbar/MobileNavbar.tsx
@@ -0,0 +1,13 @@
+import { Icon } from "@iconify-icon/react";
+
+export default function MobileNavbar() {
+  return (
+    <>
+      <section className="bg-primary h-[100px] p-4 flex items-center text-white">
+        <div>
+          <Icon icon="ic:baseline-menu" />
+        </div>
+      </section>
+    </>
+  );
+}
