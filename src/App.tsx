8d2b748ce5841830817d08310225dec02b6d7e60
Mohsen Salahshour Majd <mohsen.salahshoor130@gmail.com>
2024-07-13 10:18:37 +0330

    Added the introduction section

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import "@fontsource/ubuntu/500.css";
 import "@fontsource/ubuntu/700.css";
 import MobileNavbar from "./components/mobile-navbar/MobileNavbar";
 import DesktopNavbar from "./components/desktop-navbar/DesktopNavbar";
+import Introduction from "./components/introduction/Introduction";
 function App() {
   return (
-    <>
+    <div className="text-primary">
       <MobileNavbar />
       <DesktopNavbar />
-    </>
+      <section className="flex justify-center mx-4 mt-6">
+        <Introduction />
+      </section>
+    </div>
   );
 }
 
---
+++ src/components/introduction/Introduction.tsx
@@ -0,0 +1,19 @@
+export default function Introduction() {
+  return (
+    <p className="w-full text-justify border-[1px] border-primary rounded-xl p-3">
+      <span className="text-3xl">Hello. This is Mohsen.</span>
+      <br />
+      <br />
+      This website is a showcase of my skills and code samples regarding React.
+      <br />
+      Please feel free to explore this website and explore my skills. <br />
+      I would be happy if you could share your feedback regarding anything with
+      me through any of the contact methods mentioned in the contact section.
+      <br></br><br></br>
+      <strong>Thank you for analyzing my work,
+      <br />
+      Mohsen Majd
+      </strong>
+    </p>
+  );
+}
