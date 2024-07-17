22148ddae07131671002f43946c6a3ccbac8f232
Mohsen Salahshour Majd <mohsen.salahshoor130@gmail.com>
2024-07-17 04:21:30 +0330

    Added the routing capability to the project and completed the Home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,74 +5,22 @@ import "@fontsource/ubuntu/500.css";
 import "@fontsource/ubuntu/700.css";
 import MobileNavbar from "./components/mobile-navbar/MobileNavbar";
 import DesktopNavbar from "./components/desktop-navbar/DesktopNavbar";
-import Introduction from "./components/introduction/Introduction";
-import Challenge from "./components/challenge/Challenge";
-import stopwatchImage from "./assets/img/challenges/stopwatch.jpg";
-import Stopwatch from "./components/challenges/stopwatch/Stopwatch";
-import Progressbar from "./components/challenges/progressbar/Progressbar";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="text-primary">
-      <MobileNavbar />
-      <DesktopNavbar />
-      <section className="flex justify-center mx-4 mt-6">
-        <Introduction />
-      </section>
-
-      <section className="flex justify-center mx-4 mt-6 gap-y-8 gap-x-8 flex-wrap">
-        <Challenge
-          name="Challenge #1: Stop Watch / Timer"
-          description=" A stop watch or timer that tracks time in seconds. Has the ability to
-        start, stop, and reset."
-          img={stopwatchImage}
-          imgAlt="An image of the stopwatch challenge."
-        />
-        <Challenge
-          name="Challenge #1: Stop Watch / Timer"
-          description=" A stop watch or timer that tracks time in seconds. Has the ability to
-        start, stop, and reset."
-          img={stopwatchImage}
-          imgAlt="An image of the stopwatch challenge."
-        />
-        <Challenge
-          name="Challenge #1: Stop Watch / Timer"
-          description=" A stop watch or timer that tracks time in seconds. Has the ability to
-        start, stop, and reset."
-          img={stopwatchImage}
-          imgAlt="An image of the stopwatch challenge."
-        />
-        <Challenge
-          name="Challenge #1: Stop Watch / Timer"
-          description=" A stop watch or timer that tracks time in seconds. Has the ability to
-        start, stop, and reset."
-          img={stopwatchImage}
-          imgAlt="An image of the stopwatch challenge."
-        />
-        <Challenge
-          name="Challenge #1: Stop Watch / Timer"
-          description=" A stop watch or timer that tracks time in seconds. Has the ability to
-        start, stop, and reset."
-          img={stopwatchImage}
-          imgAlt="An image of the stopwatch challenge."
-        />
-        <Challenge
-          name="Challenge #1: Stop Watch / Timer"
-          description=" A stop watch or timer that tracks time in seconds. Has the ability to
-        start, stop, and reset."
-          img={stopwatchImage}
-          imgAlt="An image of the stopwatch challenge."
-        />
-      </section>
-
-      <section className="mx-4 mt-6 lg:w-1/3 lg:mx-auto">
-        <Stopwatch />
-      </section>
-
-      <section className="mx-4 mt-6 lg:w-1/3 lg:mx-auto">
-        <Progressbar />
-      </section>
-    </div>
+    <BrowserRouter>
+      <div className="text-primary">
+        <MobileNavbar />
+        <DesktopNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,69 @@
+import Introduction from "../introduction/Introduction";
+import Challenge from "../challenge/Challenge";
+import stopwatchImage from "../challenges/stopwatch/Stopwatch.jpg";
+import Stopwatch from "../challenges/stopwatch/Stopwatch";
+import Progressbar from "../challenges/progressbar/Progressbar";
+
+const Home = () => {
+  return (
+    <>
+      <section className="flex justify-center mx-4 mt-6">
+        <Introduction />
+      </section>
+
+      <section className="flex justify-center mx-4 mt-6 gap-y-8 gap-x-8 flex-wrap">
+        <Challenge
+          name="Challenge #1: Stop Watch / Timer"
+          description="Create a component called Stopwatch that has the ability to view and track time in hours/minutes/seconds and also the ability to start/stop/reset the tracking."
+          img={stopwatchImage}
+          imgAlt="An image of the stopwatch challenge."
+        />
+        <Challenge
+          name="Challenge #1: Stop Watch / Timer"
+          description=" A stop watch or timer that tracks time in seconds. Has the ability to
+        start, stop, and reset."
+          img={stopwatchImage}
+          imgAlt="An image of the stopwatch challenge."
+        />
+        <Challenge
+          name="Challenge #1: Stop Watch / Timer"
+          description=" A stop watch or timer that tracks time in seconds. Has the ability to
+        start, stop, and reset."
+          img={stopwatchImage}
+          imgAlt="An image of the stopwatch challenge."
+        />
+        <Challenge
+          name="Challenge #1: Stop Watch / Timer"
+          description=" A stop watch or timer that tracks time in seconds. Has the ability to
+        start, stop, and reset."
+          img={stopwatchImage}
+          imgAlt="An image of the stopwatch challenge."
+        />
+        <Challenge
+          name="Challenge #1: Stop Watch / Timer"
+          description=" A stop watch or timer that tracks time in seconds. Has the ability to
+        start, stop, and reset."
+          img={stopwatchImage}
+          imgAlt="An image of the stopwatch challenge."
+        />
+        <Challenge
+          name="Challenge #1: Stop Watch / Timer"
+          description=" A stop watch or timer that tracks time in seconds. Has the ability to
+        start, stop, and reset."
+          img={stopwatchImage}
+          imgAlt="An image of the stopwatch challenge."
+        />
+      </section>
+
+      <section className="mx-4 mt-6 lg:w-1/3 lg:mx-auto">
+        <Stopwatch />
+      </section>
+
+      <section className="mx-4 mt-6 lg:w-1/3 lg:mx-auto">
+        <Progressbar />
+      </section>
+    </>
+  );
+};
+
+export default Home;
